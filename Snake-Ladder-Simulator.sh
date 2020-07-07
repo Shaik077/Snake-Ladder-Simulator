a470216c7417a3c3753683f610436710a4e3fab9
Shaik077 <tshaikvali@gmail.com>
2020-07-07 21:20:36 +0530

    [Shaik]ADDED:Use RANDOM Function for ChecksforAOption

--- Snake-Ladder-Simulator.sh
+++ Snake-Ladder-Simulator.sh
@@ -1,8 +1,30 @@
 #!/bin/bash -x
 
-playerOnePosition=0
-rollDice(){
-	local roll=$((RANDOM%6+1))
-	echo $roll
+PlayerOnePosition=0
+RollDice(){
+	local Roll=$((RANDOM%6+1))
+	echo $Roll
 }
-rollDice
+PlayOptions(){
+	local NO_PLAY=0
+	local LADDER=1
+	local SNAKE=2
+	local Roll=$(RollDice)
+	local option=$((RANDOM%3))
+	local move=-1
+	case $option in
+		$NO_PLAY) 
+			Move=0
+			;;
+
+		$LADDER) 
+			Move=$Roll
+			;;
+		   
+		$SNAKE)
+			Move=-$Roll
+			;;
+	esac
+	echo $Move
+}
+PlayOptions
